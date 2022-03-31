b15eb2dc6d90e198cd6db4ec9d48cf1ed50395b9
Pierre-Yves Legeay <47693556+Pi-L@users.noreply.github.com>
2022-03-31 21:41:31 +0200

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+
+if (typeof(PhusionPassenger) !== 'undefined') {
+    console.log('listening... passenger')
+    PhusionPassenger.configure({ autoInstall: false });
+    http.listen('passenger');
+
+} else {
+    console.log('listening... 3000');
+    http.listen(3000);
+}
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+# base game server
+
+https://api.glop.legeay.dev/<br>
+<p>no need to reload the server (most times) just push :)</p>
